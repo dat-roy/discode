dc4c6e306e61e181be3447b77814ef2ad431b195
tronglinh23 <tronglinh230203@gmail.com>
2022-11-04 16:36:13 +0700

    Delete Member From Conversations

--- server/src/controllers/channel.controller.js
+++ server/src/controllers/channel.controller.js
@@ -1,5 +1,6 @@
 const Conversations = require("../models/conversation.model");
 const Users = require('../models/users.model');
+const Participants = require('../models/participants.model');
 class channelController {
     //[POST] /api/channel/create
     async createChannel(req, res, next) {
@@ -57,6 +58,80 @@ class channelController {
             })
         } catch (err) {
             console.log(err.message);
+            return res.status(500).json({
+                message: "An internal error from server",
+            })
+        }
+    }
+
+    async deleteMember(req, res, next) {
+        //creator_id, deleted_id, conv_id
+        //check if creator_id & deleted_id & conv_id are valid or not.
+        //check role of creator_id.
+        //check deleted_id exists in conversation or not.
+        //
+        let {creator_id, deleted_id, conv_id} = req.body;
+        try {
+            const checkCreatorIdExists =  await Users.checkExistence({where: {id: creator_id}});
+            const checkDeletedIdIdExists = await Users.checkExistence({where: {id: deleted_id}});
+            const checkConvIdExists = await Conversations.checkExistence({where: {id : conv_id}});
+            
+            if (!checkCreatorIdExists) {
+                return res.status(404).json({
+                    message: "Can not detect CreatorID"
+                })
+            }
+            if (!checkDeletedIdIdExists) {
+                return res.status(404).json({
+                    message: "Can not detect UserID"
+                })
+            }
+            if (!checkConvIdExists) {
+                return res.status(404).json({
+                    message: "Can not detect ConversationID"
+                })
+            }
+    
+            const authenCreatorId = await Conversations.findOne({
+                Attributes: [
+                    'creator_id', 'id',
+                ],
+                Where: {
+                    creator_id: creator_id,
+                    id: conv_id
+                },
+            })
+    
+            if (!authenCreatorId) {
+                return res.status(404).json({
+                    message: "ID is not Admin",
+                })
+            }
+    
+            const checkDeleteIdInConv = await Participants.findOne({
+                Attributes: [
+                    'conv_id', 'users_id',
+                ],
+                Where: {
+                    conv_id: conv_id,
+                    users_id: deleted_id,
+                },
+            })
+            
+            if (!checkDeleteIdInConv) {
+                return res.status(404).json({
+                    message: "ID want to delete is not in conversation",
+                })
+            }
+    
+            const result = await Participants.deleteMember(deleted_id, conv_id);
+            console.log(result);
+            return res.status(200).json({
+                message: "Delete Member from Conversation Successfully",
+                result: result
+            });
+        } catch (error) {
+            console.log(error.message);
             return res.status(500).json({
                 message: "An internal error from server",
             })
