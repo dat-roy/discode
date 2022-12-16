8e5e3a766431a1a6f023abd9240c174868669863
levietdat <69759053+dat-roy@users.noreply.github.com>
2022-12-16 10:35:08 +0700

    Delete server directory

--- server/src/routes/test.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const testController = require('../controllers/test.controller')
-
-router.get('/get', testController.testGet);
-router.post('/post', testController.testPost);
-
-module.exports = router;
